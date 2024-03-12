0370f9c0d26f1a69c49daba325efbebd60e3a701
Chung Tsz Ho <karmoo2012@gmail.com>
2024-03-13 02:13:06 +0800

    Create PlaylistMap.hpp

---
+++ shared/Types/BeatSaver/PlaylistMap.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "../TypeMacros.hpp"
+#include "Beatmap.hpp"
+
+namespace BeatSaver {
+    DECLARE_JSON_CLASS(PlaylistMap,
+        ERROR_CHECK
+        GETTER_VALUE(BeatSaver::Beatmap, Map, "map");
+        GETTER_VALUE(int, Order, "order");
+    )
+}
