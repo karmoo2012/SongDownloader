8d30739d1f2a5e3aec29e5e84c6d491455737efe
Chung Tsz Ho <karmoo2012@gmail.com>
2024-03-13 17:39:51 +0800

    Update PlaylistInfo.hpp

--- shared/Types/BeatSaver/PlaylistInfo.hpp
+++ shared/Types/BeatSaver/PlaylistInfo.hpp
@@ -1,1 +1,9 @@
+#pragma once
+#include "../TypeMacros.hpp"
 
+namespace BeatSaver {
+    DECLARE_JSON_CLASS(PlaylistInfo,
+        ERROR_CHECK
+        GETTER_VALUE(int, PlaylistId, "playlistId");
+    )
+}
